be8a39a1ffb030083b99afd58f7d58c2cc3d14a5
LessIsMoreMK <maciej8kz@gmail.com>
2021-03-05 23:20:30 +0100

    Custom window style

---
+++ CoinFlipper/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace CoinFlipper
+{
+    /// <summary>
+    /// A base view model that fires Property Changed events as needed
+    /// </summary>
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The event that is fired when any child property changes its value
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Call this to fire a <see cref="PropertyChanged"/> event
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        /// <summary>
+        /// Call this to fire a <see cref="PropertyChanged"/> event with lambda
+        /// </summary>
+        /// <param name="name"></param>
+        protected virtual void OnPropertyChanged<T>(Expression<Func<T>> property)
+        {
+            string propertyName = ((MemberExpression)property.Body).Member.Name;
+            OnPropertyChanged(propertyName);
+        }
+    }
+}
---
+++ CoinFlipper/ViewModels/Base/RelayCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CoinFlipper
+{
+    public class RelayCommand : ICommand
+    {
+        #region Private Members
+
+        private readonly Action mAction;
+
+        #endregion
+
+        #region Public Events
+
+        /// <summary>
+        /// The event thats fired when the <see cref="CanExecute(object)"/> value has changed
+        /// </summary>
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+        #endregion
+
+        #region Constructor
+
+        public RelayCommand(Action action)
+        {
+            mAction = action;
+        }
+
+        #endregion
+
+        #region Command Methods
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            mAction();
+        }
+
+        #endregion
+    }
+}
