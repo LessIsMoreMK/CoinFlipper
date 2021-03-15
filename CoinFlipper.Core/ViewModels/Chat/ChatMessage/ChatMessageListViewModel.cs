6e155472bc979b886856d98d2b605210eea0656f
LessIsMoreMK <maciej8kz@gmail.com>
2021-03-15 13:03:44 +0100

    Chat bubbles progress

---
+++ CoinFlipper.Core/ViewModels/Chat/ChatMessage/ChatMessageListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace CoinFlipper.Core
+{
+    /// <summary>
+    /// A view model for the overview chat message thread list
+    /// </summary>
+    public class ChatMessageListViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// The chat thread items for the list  
+        /// </summary>
+        public List<ChatMessageListItemViewModel> Items { get; set; }
+
+    }
+}
---
+++ CoinFlipper.Core/ViewModels/Chat/ChatMessage/Design/ChatMessageListDesignModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoinFlipper.Core
+{
+    /// <summary>
+    /// The design-time data for a <see cref="ChatMessageListViewModel"/>
+    /// </summary>
+    public class ChatMessageListDesignModel : ChatMessageListViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static ChatMessageListDesignModel Instance => new ChatMessageListDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public ChatMessageListDesignModel()
+        {
+            Items = new List<ChatMessageListItemViewModel>
+            {
+               new ChatMessageListItemViewModel
+               {
+                   SenderName = "Alice",
+                   Initials = "AS",
+                   Message = "We need to talk. Call me as soon as possible.",
+                   ProfilePictureRGB = "3275A8",
+                   MessageSentTime = DateTimeOffset.UtcNow,
+                   SentByMe = false,
+               },
+               new ChatMessageListItemViewModel
+               {
+                   SenderName = "Maciej",
+                   Initials = "MK",
+                   Message = "I'm on a meeting, can't right now. Should finish before 14",
+                   ProfilePictureRGB = "3275A8",
+                   MessageSentTime = DateTimeOffset.UtcNow,
+                   MessageReadTime = DateTimeOffset.UtcNow.Subtract(TimeSpan.FromDays(1.3)),
+                   SentByMe = true,
+               },
+               new ChatMessageListItemViewModel
+               {
+                   SenderName = "Alice",
+                   Initials = "AS",
+                   Message = "Okay, waiting impatiently",
+                   ProfilePictureRGB = "3275A8",
+                   MessageSentTime = DateTimeOffset.UtcNow,
+                   SentByMe = false,
+               },
+            };
+        }
+        
+        #endregion
+    }
+}
