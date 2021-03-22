f61af1d0ce51e29359b20025af0cba26ddc102c4
LessIsMoreMK <maciej8kz@gmail.com>
2021-03-22 18:03:47 +0100

    Settings page

---
+++ CoinFlipper/AttachedProperties/PanelChildMarginProperty.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CoinFlipper
+{
+    /// <summary>
+    /// The NoFrameHistory attached property for creating a <see cref="Frame"/> that never shows navigation
+    /// and keep the navigation history empty
+    /// </summary>
+    public class PanelChildMarginProperty : BaseAttachedProperty<PanelChildMarginProperty, string>
+    {
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            // Get the panel (grid typically)
+            var panel = (sender as Panel);
+
+            // Wait for panel to load
+            panel.Loaded += (s, e3) =>
+            {
+                // Loop each child
+                foreach(var child in panel.Children)
+                {
+                    // Set it's margin to the given value
+                    (child as FrameworkElement).Margin = (Thickness)(new ThicknessConverter().ConvertFromString(e.NewValue as string));
+                }
+            };
+        }
+    }
+}
+ 
